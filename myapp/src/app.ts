2c4c4fd5166b874a2cb90d6dc4f6c8229697a724
apple <apple@apples-Air.lan>
2024-10-02 15:41:46 +0530

    Docker Compose File Implementation

---
+++ myapp/src/app.ts
@@ -0,0 +1,66 @@
+import express from 'express'
+import cors from 'cors'
+import { errorMiddleware } from './middlewares/error.js'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import mongoose, { Mongoose } from 'mongoose'
+
+dotenv.config({ path: './.env', });
+
+export const envMode = process.env.NODE_ENV?.trim() || 'DEVELOPMENT';
+const port = process.env.PORT || 3000;
+const URI = process.env.MONGO_URI;
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI!, {
+  dbName: "Docker_DB"
+})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: ' * ', credentials: true }));
+app.use(morgan('dev'))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// your routes here
+
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Page not found'
+  });
+});
+
+// app.use(errorMiddleware);
+
+
+app.listen(port, () => console.log('Server is working on Port:' + port + ' in ' + envMode + ' Mode.'));
+
+const createUser = async (name: String, email: String) => {
+  const schema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    }
+    ,
+    email: {
+      type: String,
+      required: true
+    }
+  });
+  const User = mongoose.model("User", schema);
+
+  await User.create({ name, email });
+  console.log("user is created")
+}
+
+await createUser("jayank", "jayank@gmail.com");
